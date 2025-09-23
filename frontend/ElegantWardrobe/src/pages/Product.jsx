d23a867da888053319fa1099a4908d142b78e969
Sabarinath E M <sabarinathem36@gmail.com>
2025-09-23 14:46:39 +0530

    added dynamic rendering in products

--- frontend/ElegantWardrobe/src/pages/Product.jsx
+++ frontend/ElegantWardrobe/src/pages/Product.jsx
@@ -258,7 +258,7 @@ const Product = () => {
             {/* Product Images */}
             <div className="flex-1 flex flex-col-reverse gap-3 sm:flex-row">
               <div className="flex sm:flex-col overflow-x-auto sm:overflow-y-scroll justify-between sm:justify-normal sm:w-[18.7%] w-full max-h-[500px]">
-                {productData.images.map((item, index) => (
+               {productData.images && productData.images.map((item, index) => (
                   <img
                     onClick={() => {
                       setImage(item);
