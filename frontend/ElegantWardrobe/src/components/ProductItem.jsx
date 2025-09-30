6714ce5f38aac8898f08901f4f6e43db2d257125
Sabarinath E M <sabarinathem36@gmail.com>
2025-09-30 20:14:59 +0530

    update hover color in ProductItem component from red to green

--- frontend/ElegantWardrobe/src/components/ProductItem.jsx
+++ frontend/ElegantWardrobe/src/components/ProductItem.jsx
@@ -202,7 +202,7 @@ const ProductItem = ({
               strokeWidth="2"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className="hover:stroke-red-500 transition-colors duration-200"
+              className="hover:stroke-green-500 transition-colors duration-200"
             >
               <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"></path>
             </svg>
